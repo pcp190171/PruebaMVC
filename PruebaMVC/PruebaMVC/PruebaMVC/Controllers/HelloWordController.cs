7e5446b53734a995d4205bbd5b280d2dae5a79dc
Pedro Carrillo Pacheco <pcarrillopacheco@gmail.com>
2015-08-31 18:41:22 +0200

    Nuevo controller 'HelloWord'

---
+++ PruebaMVC/PruebaMVC/PruebaMVC/Controllers/HelloWordController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PruebaMVC.Controllers
+{
+    public class HelloWordController : Controller
+    {
+        //
+        // GET: /HelloWord/
+
+        public string Index()
+        {
+            return "My default action ...";
+        }
+
+        public string Welcome()
+        {
+            return "Welcome action.";
+        }
+
+    }
+}
